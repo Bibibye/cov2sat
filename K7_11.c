3e4faadc7d600da842a66440eee5c40d51ed4ee6
Bibibye <Bibibye@users.noreply.github.com>
2014-11-21 20:16:18 +0100

    Correction in K7_11

--- K7_11.c
+++ K7_11.c
@@ -2,7 +2,7 @@ int nb_vertices(){
 return 18;}
 
 int is_adjacent(int u, int v){
- if(0< u && 0<v && u<18 && v<18){
+ if(0< u && 0<v && u<19 && v<19){
   return (u<12 && v>11);
  }	
  else return 0;
